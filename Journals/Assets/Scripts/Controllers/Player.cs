d4dbed15512a6a781f7dbd6fd91f91ef782fd57e
OUTOFTHEBLUE\whate <whateverfloatsyourboat077@gmail.com>
2025-09-17 15:54:07 -0400

    In class reupload

---
+++ Journals/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            SpawnBombAtOffset(new Vector3(0, 1));
+        }
+
+    }
+
+    private void SpawnBombAtOffset(Vector3 inOffset) //Added private
+    {
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+        //Remember that transformpoition refers to whatever object this is attached
+    }
+
+}
