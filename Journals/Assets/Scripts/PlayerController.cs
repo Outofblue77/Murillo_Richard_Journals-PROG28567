eb3990124c32ff8c46017b4cd91a5e5bbb8a08af
Outofblue77 <whateverfloatsyourboat077@gmail.com>
2025-11-21 15:14:38 -0500

    Completion of Task 1. Added jump Mechanic where turns off gravity. Reassigned Ground check to layer of tiles with empty under player.

--- Journals/Assets/Scripts/PlayerController.cs
+++ Journals/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,34 @@ public class PlayerController : MonoBehaviour
         left, right
     }
 
+    //movement
     public float moveSpeed = 5f;
 
+    //Ground Check
     public Transform groundCheck;
     public float groundRadius = 0.1f;
     public LayerMask groundLayer;
 
+    //Rigid Body
     private Rigidbody2D rb;
     private float currentHorizontalInput = 0f;
     private FacingDirection facing = FacingDirection.right;
 
+    //Jump Settings
+    public float apexHeight = 3.5f;
+    public float apexTime = 0.5f;
+
+    //Gravity
+    private float gravity;
+    private float jumpVelocity;
+    private bool isJumping = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        gravity = -2 * apexHeight / (apexTime * apexTime);
+        jumpVelocity = (2 * apexHeight) / apexTime;
     }
 
     // Update is called once per frame
@@ -30,7 +45,7 @@ public class PlayerController : MonoBehaviour
         // manage the actual movement of the character.
         //Vector2 playerInput = new Vector2();
 
-        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetButtonDown("Jump") ? 1 : 0);
         MovementUpdate(playerInput);
     }
 
@@ -45,8 +60,32 @@ public class PlayerController : MonoBehaviour
             facing = FacingDirection.right;
         else if (currentHorizontalInput < 0)
             facing = FacingDirection.left;
+
+        JumpUpdate(playerInput.y);
     }
 
+    private void JumpUpdate (float jumpPressed)
+    {
+        bool grounded = IsGrounded();
+
+        if (grounded && jumpPressed == 1 && !isJumping)
+        {
+            isJumping = true;
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpVelocity);
+        }
+
+        if (isJumping)
+        {
+            rb.linearVelocity += new Vector2(0, gravity * Time.deltaTime);
+
+            if (rb.linearVelocity.y <= 0)
+            {
+                rb.gravityScale = 1f;
+                isJumping = false;
+            }
+        }
+    }
     public bool IsWalking()
     {
         return Mathf.Abs(currentHorizontalInput) > 0.01f;
